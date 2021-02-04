b784bbd1d86d6dfdefe4a27899c420b0d3972703
Tibo-Wild <thibault.leparc45@gmail.com>
2021-02-04 16:46:15 +0100

    add match routes

--- src/routes/previous.route.js
+++ src/routes/previous.route.js
@@ -2,7 +2,7 @@ const {connection} = require('../db_connection');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  const sql = "SELECT * FROM previous_match";
+  const sql = "SELECT * FROM previous_match ORDER BY id DESC";
   connection.query(sql, (err, results) => {
     if (err) {
       res.status(500).send({errorMessage: err.message});
