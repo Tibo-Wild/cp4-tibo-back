84bdd351bb228ff464117b857671cfd1a7ae934c
Tibo-Wild <thibault.leparc45@gmail.com>
2021-02-04 15:45:36 +0100

    add next matchs and previous matchs routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const adminRouter = require('./admin.routes.js');
 const playersRouter = require('./players.route.js');
 const trainerRouter = require ('./trainer.routes.js');
+const nextRouter = require ('./next.routes.js');
+const previousRouter = require('./previous.route.js');
 
 router.use('/admins', adminRouter);
 router.use('/players', playersRouter);
 router.use('/trainer', trainerRouter);
+router.use('/next', nextRouter);
+router.use('/previous', previousRouter);
 
 module.exports = router;
